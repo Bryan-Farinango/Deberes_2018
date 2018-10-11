462ca140e8b207e74cc3818f0518718663497ff2
Bryan-Farinango <38628690+Bryan-Farinango@users.noreply.github.com>
2018-10-11 00:57:25 -0500

    Add files via upload

---
+++ ejerciciosDeber1.py
@@ -0,0 +1,18 @@
+#print("ingrese su edad")
+#x = input()
+#print("Tienes: " + x)
+#x = int(x)
+
+
+
+print ("Ingrese la edad de a persona")
+a = input()
+a = int(a)
+print("Ingrese la edad de b persona")
+b = input()
+b = int(b)
+
+if a > b :
+  print("El mayo es a")
+else:
+  print("El mayor es b")
